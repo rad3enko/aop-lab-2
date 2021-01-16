ab3b1d7e205a28302691aaa82f21056357b6c050
Sergey Radchenko <sradchenko@tectus-it.ru>
2021-01-16 15:31:21 +0300

    fix component scan, app context

--- src/main/java/ru/sdo/ism91z/radchenko/main/Start.java
+++ src/main/java/ru/sdo/ism91z/radchenko/main/Start.java
@@ -1,12 +1,15 @@
 package ru.sdo.ism91z.radchenko.main;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.sdo.ism91z.radchenko.model.robot.Robot;
 
+@ComponentScan(basePackages = "ru.sdo.ism91z.radchenko.config, ru.sdo.ism91z.radchenko.model")
 public class Start {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Start.class);
         Robot t1000robot = (Robot) context.getBean("t1000robot");
         t1000robot.fire();
     }
